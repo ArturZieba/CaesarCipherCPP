ee4215b73162bed699e3812071961732633ea3bb
Siwy <43797135+ArturZieba@users.noreply.github.com>
2024-05-31 22:47:00 +0200

    Add code for writing data into a .txt file
    - Add simple code for writing a string into a .txt file.
    - Verify the file is saved with expected string.

--- Caesar.cpp
+++ Caesar.cpp
@@ -1,7 +1,11 @@
 #include "Caesar.h"
+#include <fstream> // For std::ofstream, std::ifstream, and std::fstream
 #include <iostream> // For std::cout
 
 int main()
 {
-	std::cout << "Ave Imperator!";
+	std::ofstream outputFile; // Output file stream object, only for writing data into a file
+	outputFile.open("outputFile.txt"); // Open a file with provided name
+	outputFile << "Test"; // Write data into the stream object
+	outputFile.close(); // Closes the file associated with the stream object
 }
