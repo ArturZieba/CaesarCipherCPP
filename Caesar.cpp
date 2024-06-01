b8ca73dc313769a299c1c91242775c93f0da88df
Siwy <43797135+ArturZieba@users.noreply.github.com>
2024-06-01 16:21:16 +0200

    Add a code for reading data from a file
    - Add a code that opens ifstream on a file created earlier.
    - Add a code that writes ifstream contents into stringstream and passes it into std::string variable.
    - Verify that adding modifications to the file is reflected in output.

--- Caesar.cpp
+++ Caesar.cpp
@@ -1,11 +1,22 @@
 #include "Caesar.h"
 #include <fstream> // For std::ofstream, std::ifstream, and std::fstream
 #include <iostream> // For std::cout
+#include <sstream> // For std::stringstream
+#include <string> // For std::string
+
 
 int main()
 {
-	std::ofstream outputFile; // Output file stream object, only for writing data into a file
-	outputFile.open("outputFile.txt"); // Open a file with provided name
-	outputFile << "Test"; // Write data into the stream object
-	outputFile.close(); // Closes the file associated with the stream object
+	std::ofstream outputStream; // Output file stream object, only for writing data into a file
+	outputStream.open("outputStream.txt"); // Open a stream for a file with provided name
+	outputStream << "Test123tseT"; // Write data into the stream object
+	outputStream.close(); // Closes the stream
+
+	std::ifstream inputStream; // Input file stream object, only for reading data from a file
+	inputStream.open("outputStream.txt"); // Open a stream for a file with provided name
+	std::stringstream stringStream; // String stream object
+	stringStream << inputStream.rdbuf(); // Write input stream buffer content into the string stream
+	std::string inputString = stringStream.str(); // Write string stream content into a string variable
+	std::cout << inputString; 
+	inputStream.close(); // Closes the stream
 }
