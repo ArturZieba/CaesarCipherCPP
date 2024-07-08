714a1a6afcf09f4067de6b43b9887df66ddb5420
Siwy <43797135+ArturZieba@users.noreply.github.com>
2024-07-08 18:33:02 +0200

    Separate switch statements for input and output selection
    - Split input and output selection into two switch statements.
    - Add placeholder statements for all functions.
    - Specify which option calls which function in the print statements.
    - Verify all switch statement cases redirect to expected functions.

--- Caesar.cpp
+++ Caesar.cpp
@@ -28,7 +28,7 @@ std::string inputTextFile()
 // TODO: Output can be returned to the Command Prompt
 void outputCommandPrompt(std::string inputData)
 {
-
+	std::cout << "Command Prompt output" << '\n';
 }
 
 // TODO: Output can be returned as a text file
@@ -46,50 +46,73 @@ void outputTextFile(std::string inputData)
 // TODO: Brute-force option that writes out all possible 0 - 25 shifts for the entered string to the Command Prompt
 void outputAllPossibleCiphersCommandPrompt(std::string inputData)
 {
-
+	std::cout << "Output all possible ciphers to the Command Prompt" << '\n';
 }
 
 // TODO: Brute-force option that writes out all possible 0 - 25 shifts for the entered string to a text file
 void outputAllPossibleCiphersTextFile(std::string inputData)
 {
-
+	std::cout << "Output all possible ciphers to a text file" << '\n';
 }
-
 
 int main()
 {
 	// Caesar cipher works by replacing (shifting) letters with other letters to their left by 0 - 25. For example A shifted by 0 is A; shifted by 2 is Y; shifted by 25 is B.
 	// Code implementation of this mechanic would require an input string, shift number, and outputting the modified string.
 
-	std::cout << "Type 0 to for selection 0" << '\n';
-	std::cout << "Type 1 to for selection 1" << '\n';
-	std::cout << "Type 2 to for selection 2" << '\n';
-	std::cout << "Type 3 to for selection 3" << '\n';
-	std::cout << "Type 4 to for selection 4" << '\n';
+	std::cout << "Choose input type: " << '\n';
+	std::cout << "Type 0 to use Command Prompt for input" << '\n';
+	std::cout << "Type 1 to use a text file for input" << '\n';
 
-	int userSelection;
+	int userInputSelection;
 
 	std::cout << '\n' << "Your selection: ";
-	std::cin >> userSelection;
+	std::cin >> userInputSelection;
 	std::cout << '\n';
 
-	switch (userSelection)
+	switch (userInputSelection)
 	{
 	case 0:
 		std::cout << "Chose 0" << '\n';
-		outputTextFile(inputCommandPrompt());
+		inputCommandPrompt();
 		break;
 	case 1:
 		std::cout << "Chose 1" << '\n';
+		inputTextFile();
+		break;
+	default:
+		std::cout << "Not listed input" << '\n'; //TODO: Return to selection? ("Unsupported input, please try again:")
+	}
+
+	std::cout << "Choose output type: " << '\n';
+	std::cout << "Type 0 to use Command Prompt for output" << '\n';
+	std::cout << "Type 1 to use a text file for output" << '\n';
+	std::cout << "Type 2 to output all possible ciphers into the Command Prompt" << '\n';
+	std::cout << "Type 3 to output all possible ciphers into a text file" << '\n';
+
+	int userOutputSelection;
+
+	std::cout << '\n' << "Your selection: ";
+	std::cin >> userOutputSelection;
+	std::cout << '\n';
+
+	switch (userOutputSelection)
+	{
+	case 0:
+		std::cout << "Chose 0" << '\n';
+		outputCommandPrompt("Option 0");
+		break;
+	case 1:
+		std::cout << "Chose 1" << '\n';
+		outputTextFile("Option 1");
 		break;
 	case 2:
 		std::cout << "Chose 2" << '\n';
+		outputAllPossibleCiphersCommandPrompt("Option 2");
 		break;
 	case 3:
 		std::cout << "Chose 3" << '\n';
-		break;
-	case 4:
-		std::cout << "Chose 4" << '\n';
+		outputAllPossibleCiphersTextFile("Option 3");
 		break;
 	default:
 		std::cout << "Not listed input" << '\n'; //TODO: Return to selection? ("Unsupported input, please try again:")
